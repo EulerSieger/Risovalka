abd800b237a3c7e4216e20acb0d94c00bd6cceb7
EulerSieger <sergantsergey@gmail.com>
2022-12-14 19:09:00 +0300

    tools

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import canvasState from "../store/canvasState";
+import toolState from "../store/toolState";
 import "../styles/toolbar.scss"
+import Brush from "../tools/Brush";
+import Circle from "../tools/Circle";
+import Eriser from "../tools/Eriser";
+import Line from "../tools/Line";
+import Rect from "../tools/Rect";
 
 const Toolbar= () =>{
   return (
     <div className="toolbar">
-      <button className="toolbar__btn brush" />
-      <button className="toolbar__btn rect" />
-      <button className="toolbar__btn circle" />
-      <button className="toolbar__btn eraser" />
-      <button className="toolbar__btn line" />
+      <button className="toolbar__btn brush" onClick={()=> toolState.setTool(new Brush(canvasState.canvas))} />
+      <button className="toolbar__btn rect" onClick={()=> toolState.setTool(new Rect(canvasState.canvas))} />
+      <button className="toolbar__btn circle" onClick={()=> toolState.setTool(new Circle(canvasState.canvas))} />
+      <button className="toolbar__btn eraser" onClick={()=> toolState.setTool(new Eriser(canvasState.canvas))} />
+      <button className="toolbar__btn line" onClick={()=> toolState.setTool(new Line(canvasState.canvas))}/>
       <input type="color" className="toolbar__btn color"/>
       <button className="toolbar__btn undo" />
       <button className="toolbar__btn redo" />
---
+++ src/tools/Tools.js
@@ -0,0 +1,16 @@
+export default class Tool {
+    
+    constructor(canvas){
+
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d')
+
+        this.destroyEvents()
+    }
+
+        destroyEvents(){
+            this.canvas.onmouseup = null
+            this.canvas.onmousedown = null
+            this.canvas.onmousemove = null
+        }
+}
